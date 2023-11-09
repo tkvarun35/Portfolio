ac6cc056d6e3bc98d98fcfe692b8ecfbb0d44402
Varun <varun@ERPs-MacBook-Pro.local>
2023-11-09 13:05:36 +0530

    Fixed dark mode in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,8 +4,8 @@ import SVG from "@/public/404.svg";
 export default function NotFound() {
   return (
     <>
-      <div className="h-screen w-screen bg-white flex items-center">
-        <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+      <div className="h-screen w-screen bg-white dark:bg-[#121212]  flex items-center">
+        <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700 dark:text-gray-50">
           <div className="max-w-md">
             <div className="text-5xl font-dark font-bold">404</div>
             <p className="text-2xl md:text-3xl font-light leading-normal">
@@ -18,7 +18,7 @@ export default function NotFound() {
 
             <a href="/">
               {" "}
-              <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
+              <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-xl focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
                 Back to homepage
               </button>
             </a>
