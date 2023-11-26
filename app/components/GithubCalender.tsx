ac761675df43858ac7fed46fa75d3960a194b879
Varun <varun@ERPs-MacBook-Pro.local>
2023-11-26 19:22:10 +0530

    Added theme and tooltip in github contributions

--- app/components/GithubCalender.tsx
+++ app/components/GithubCalender.tsx
@@ -2,8 +2,13 @@
 import GitHubCalendar from "react-github-calendar";
 import { edu_tas } from "../fonts/fonts";
 import data from "@/Details.json";
+import { cloneElement } from "react";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import { useTheme } from "next-themes";
 
 function GithubCalender() {
+  const { resolvedTheme } = useTheme();
   return (
     <div className={edu_tas.className}>
       <div className=" space-y-10">
@@ -11,7 +16,28 @@ function GithubCalender() {
           My Github Contributions ⭐️
         </p>
         <div className="flex justify-center">
-          <GitHubCalendar username={data.githubUsername} colorScheme="light" />
+          <GitHubCalendar
+            username={data.githubUsername}
+            colorScheme={resolvedTheme === "dark" ? "dark" : "light"}
+            showWeekdayLabels={true}
+            theme={{
+              light: ["#f0f0f0", "#87e2bf", "#4b8583", "#f73859", "#384259"],
+              dark: [
+                "hsl(0, 0%, 22%)",
+                "#69578b",
+                "#7DB9B6",
+                "#F5E9CF",
+                "#E96479",
+              ],
+            }}
+            renderBlock={(block, activity) =>
+              cloneElement(block, {
+                "data-tooltip-id": "react-tooltip",
+                "data-tooltip-html": `${activity.count} activities on ${activity.date}`,
+              })
+            }
+          />
+          <ReactTooltip id="react-tooltip" />
         </div>
       </div>
     </div>
