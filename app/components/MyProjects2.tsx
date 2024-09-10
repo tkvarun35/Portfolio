12fa9cdd32aa678abd82faa1211fa3480b462410
tkvarun35 <tkvarun35@gmail.com>
2024-09-11 00:58:58 +0530

    fix

--- app/components/MyProjects2.tsx
+++ app/components/MyProjects2.tsx
@@ -7,7 +7,7 @@ import ProjectCard from "./ProjectCard";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 import useEmblaCarousel from "embla-carousel-react";
-import { EmblaOptionsType} from 'embla-carousel';
+import { EmblaOptionsType } from 'embla-carousel';
 import AutoPlay from "embla-carousel-autoplay"
 import { db } from "@/firebase/config";
 import { edu_tas } from "../fonts/fonts";
@@ -73,36 +73,43 @@ function EnhancedProjectSlideshow() {
 
     return (
         <>
-            <div className="p-10 flex justify-center space-y-0 " id="project">
-                <div className={edu_tas.className}>
-                    <p className="text-3xl font-bold "> My Projects</p>
-                </div>
-            </div>
-            <div className="relative w-full max-w-5xl mx-auto px-4 py-8">
-                <div className="relative overflow-hidden" ref={emblaRef}>
-                    <div className="flex">
-                        {projects.map((project, id) => {
-                            return <ProjectCard key={id} {...project} />
-
-                        })}
+            {projects ? (
+                <>
+                    <div className="p-10 flex justify-center space-y-0 " id="project">
+                        <div className={edu_tas.className}>
+                            <p className="text-3xl font-bold "> My Projects</p>
+                        </div>
                     </div>
-                </div>
-                <button
-                    onClick={scrollPrev}
-                    className="absolute left-4 md:-left-10 top-1/2 -translate-y-1/2 bg-white rounded-full p-2 shadow-md  md:block"
-                >
-                    <FaAngleLeft className="w-6 h-6 " />
-                </button>
-                <button
-                    onClick={scrollNext}
-                    className="absolute right-4 md:-right-10 top-1/2 -translate-y-1/2 bg-white rounded-full p-2 shadow-md  md:block"
-                >
+                    <div className="relative w-full max-w-5xl mx-auto px-4 py-8">
+                        <div className="relative overflow-hidden" ref={emblaRef}>
+                            <div className="flex">
+                                {projects.map((project, id) => {
+                                    return <ProjectCard key={id} {...project} />
 
-                    <FaAngleRight className="w-6 h-6" />
-                </button>
+                                })}
+                            </div>
+                        </div>
+                        <button
+                            onClick={scrollPrev}
+                            className="absolute left-4 md:-left-10 top-1/2 -translate-y-1/2 bg-white rounded-full p-2 shadow-md  "
+                        >
+                            <FaAngleLeft className="w-6 h-6 " />
+                        </button>
+                        <button
+                            onClick={scrollNext}
+                            className="absolute right-4 md:-right-10 top-1/2 -translate-y-1/2 bg-white rounded-full p-2 shadow-md  "
+                        >
 
-            </div>
-            <hr className="h-px w-10/12 my-8 mx-auto bg-gray-200 border-0 dark:bg-gray-700" />
+                            <FaAngleRight className="w-6 h-6" />
+                        </button>
+
+                    </div>
+                    <hr className="h-px w-10/12 my-8 mx-auto bg-gray-200 border-0 dark:bg-gray-700" />
+                </>
+            ) : (
+                <></>
+            )}
+
         </>
 
     )
